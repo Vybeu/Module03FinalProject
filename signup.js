ac81814ac194d3a4931193aa842f134a231bc85a
Vybeu <chandlerhigs@gmail.com>
2022-11-18 12:20:57 -0700

    Fully added login functionality, removed TOS, added account html file with hello world.

--- signup.js
+++ signup.js
@@ -1,9 +1,10 @@
 // let signUpEl = document.getElementById('sign-up');
 // let signInEl = document.getElementById('sign-in');
 
+let totalUsers = [];
+let usersObj = {};
 
 function signUp() {
-    let usersObj = {};
     let name = document.getElementById('userName').value;
     let email = document.getElementById('userEmail').value;
     let password = document.getElementById('userPassword').value;
@@ -18,10 +19,21 @@ function signUp() {
         usersObj.name = name;
         usersObj.email = email;
         usersObj.password = password;
+        //totalUsers.push(usersObj);
         console.log(usersObj);
-
         document.getElementById('sign-up').style.display = 'none';
         document.getElementById('sign-in').style.display = 'block';
     }
 
+}
+
+function logIn() {
+    let email = document.getElementById('users-email').value;
+    let password = document.getElementById('users-password').value;
+
+    if (email === usersObj.email && password === usersObj.password) {
+        window.location.href = 'http://127.0.0.1:5500/account.html';
+    } else {
+        alert('Incorrect email or password!');
+    }
 }
